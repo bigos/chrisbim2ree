19624ae4500396c1c099dc0e12aa0fd924120200
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-20 00:08:48 +0100

    now I can photos to shopping basket

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class CartItemsController < InheritedResources::Base
+  actions :all, :except => [ :new]
+  def index
+    if current_admin
+      @cart_items = CartItem.all
+    else
+      @cart_items = CartItem.where(:shopping_cart_id => @shopping_cart.id)
+    end
+    index!
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ActiveRecord::Base
   attr_accessible :option, :photo_id, :price, :quantity, :shopping_cart_id
 
   belongs_to :shopping_cart
-  validates_presence_of :shopping_cart_id, :photo_id, :quantity, :price, :option
+  validates_presence_of :shopping_cart_id, :photo_id, :quantity, :option
   validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 1
 end
