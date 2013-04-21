4e3999b3e549a2fa91dbde2817c1913971d7c618
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-22 00:27:00 +0100

    removed _

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Chrisbim2ree::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :address              => secret['email']['production']["address"],
     :port                 => 26,
-    :user_name            => secret['email']['production']["username"],
+    :username            => secret['email']['production']["username"],
     :password             => secret['email']['production']["password"],
     :authentication       => :login,
     :enable_starttls_auto => false
