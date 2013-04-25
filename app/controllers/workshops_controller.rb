4fb2bd3bf16391981da7fe8649f076ad0e1e722d
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-25 22:24:40 +0100

    fixed workshops listing

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,3 +1,11 @@
 class WorkshopsController < InheritedResources::Base
   before_filter :require_admin, :except => [:index, :show]
+  def index
+    if params[:duration]
+      @workshops = Workshop.where(:duration => params[:duration])
+    else
+      @workshops = Workshop.find(:all)
+    end
+    index!
+  end
 end
