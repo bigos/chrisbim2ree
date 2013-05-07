a656ee2b53b201243b78f76cb40bb597d89e2e45
Jacek Podkanski <ruby.object@googlemail.com>
2013-05-07 17:50:57 +0100

    changed posts order in blog index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
 class PostsController < InheritedResources::Base
   before_filter :require_admin, :except => [:index, :show]
+  def index
+    @posts = Post.order('created_at DESC')
+    index!
+  end
 end
