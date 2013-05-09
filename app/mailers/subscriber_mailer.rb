84f51eb84855e16b0edd4269ffdaf83af49f2172
Jacek Podkanski <ruby.object@googlemail.com>
2013-05-09 23:44:31 +0100

    trying again

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -17,6 +17,7 @@ class SubscriberMailer < ActionMailer::Base
     end
     
     @subscriber = subscriber
+    logger.info @@secrets.inspect
     mail(:to => @@secrets['email']['admin_email'],
          #:cc => @@secrets['email']['cc'],
          :subject => "You have new subscriber")
