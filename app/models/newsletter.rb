6a6fe85d4724fcb3f1ed68a148dd6610959ae025
Jacek Podkanski <ruby.object@googlemail.com>
2013-05-25 14:41:50 +0100

    going to try it live

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -6,7 +6,7 @@ class Newsletter < ActiveRecord::Base
     recipients = NewsletterSubscriber.all
     recipients.each do |recipient|
       if mode == 'test'
-        if recipient.email.index 'jacek'
+        if recipient.email.index('jacek') or (recipient.email.index('chris') and recipient.email.index('beard'))
           logger.info "going to send to " + recipient.email
           NewsletterMailer.newsletter(self, recipient.email).deliver
         end
