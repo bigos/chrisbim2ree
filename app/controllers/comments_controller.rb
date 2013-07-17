2e071cc423e2333595d4936baaa067ba7c4d15fd
Jacek Podkanski <ruby.object@googlemail.com>
2013-07-17 15:16:03 +0100

    added reditecting back to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,6 @@
 class CommentsController < InheritedResources::Base
   before_filter :require_admin, :only => [:edit, :update, :destroy]
+  def create
+    create! {post_url :id => @comment.post_id}
+  end
 end  
