f077f8b07d2eeb9983fd6f8bc12e13aa47e1dfdd
Jacek Podkanski <ruby.object@googlemail.com>
2013-08-15 11:47:21 +0100

    added captcha for blog comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,26 @@ class CommentsController < InheritedResources::Base
   before_filter :require_admin, :only => [:edit, :update, :destroy]
   def create
     
-    create! (:notice => "Your comment has been created and will be shown on this page apon approval") {post_url :id => @comment.post_id}
+    # create! (:notice => "Your comment has been created and will be shown on this page apon approval") {post_url :id => @comment.post_id}
+    @comment = Comment.new(params[:comment])
+
+    if verify_recaptcha(:model => @comment, :message => "Oh! It's error with reCAPTCHA!") 
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to @comment, :notice => 'Comment was successfully created and will be displayed after approval.' }
+          format.json { render :json => @comment, :status => :created, :location => @comment }
+        else
+          format.html { render :action => "new" }
+          format.json { render :json => @comment.errors, :status => :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { render :action => "new" }
+        format.json { render :json => @comment.errors, :status => :unprocessable_entity }
+      end
+    end    
+
   end
   def destroy
     destroy! {
