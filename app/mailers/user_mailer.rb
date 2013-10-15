bdff4d0f14908771fcd7f5f91f1c958d788ff785
Jacek Podkanski <ruby.object@googlemail.com>
2013-10-15 10:57:16 +0100

    corrected the link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   @@secrets = YAML.load_file( "#{ Rails.root}/config/secret.yml")
-  if Rails.env == 'development' or  Rails.env == "test" 
+  if Rails.env == 'development' or  Rails.env == "test"
     @@url = "http://localhost:3000"
   else
-    @@url = "http://www.chrisbeard-photography.co.uk"
+    @@url = "http://chrisbeard-images.com"
   end
   default :from => @@secrets['email_server']['user_name']
 
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
          #:cc => @@secrets['email']['cc'],
          :subject => @message.subject)
   end
-  
+
   def welcome_email(user)
     @user = user
     mail(:to => user.email,
@@ -34,6 +34,6 @@ class UserMailer < ActionMailer::Base
     @url = @@url
     @reset_path = edit_password_reset_path(user.perishable_token)
     mail(:to => user.email,
-         :subject => "Password Reset Instructions")    
+         :subject => "Password Reset Instructions")
   end
 end
