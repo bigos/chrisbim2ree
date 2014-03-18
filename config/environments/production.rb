1ca716d4ea0890f6c0258e4dbf6af7e47b4467c6
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-18 01:54:47 +0000

    added one gem and changed asset configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,3 @@
-
 Chrisbim2ree::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -10,7 +9,7 @@ Chrisbim2ree::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = false
