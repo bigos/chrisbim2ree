bcb9cb297faf1e9b58e6c7647c02ab6dc0afca59
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-25 11:00:00 +0000

    modified unicorn_init.sh

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,15 @@ secrets = YAML.load_file( "#{ File.dirname(__FILE__)}/secret.yml")
 set(:user) { secrets['deployment']['user'] }
 set(:password) { secrets['deployment']['password'] }
 set :application, "chrisbim2ree"
-set :app_path, "/home/#{user}/testCapistrano/#{application}"
+set :app_path, "/home/#{user}/Rails/#{application}"
 
 set :scm, "git"
 set :branch, "master"
 set :repository,  "git@github.com:bigos/chrisbim2ree.git"
 set :deploy_via, :remote_cache
-set :deploy_to, "/home/#{user}/testCapistrano/#{application}"
+set :ssh_options, { :forward_agent => true }
+
+set :deploy_to, "#{app_path}"
 set :use_sudo, false
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -33,7 +35,8 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "chmod +x #{app_path}/shared/config/unicorn_init.sh"
+      run "sh #{app_path}/current/config/unicorn_init.sh #{command}"
     end
   end
 
@@ -46,4 +49,29 @@ namespace :deploy do
     end
   end
   before "deploy", "deploy:check_revision"
+end
+
+
+namespace :deploy do
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
+  end
+  task :finalize_update, :except => {:no_release => true} do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/ckeditor_assets #{latest_release}/public/ckeditor_assets &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml
+    CMD
+  end
 end
