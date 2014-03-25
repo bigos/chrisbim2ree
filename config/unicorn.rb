583eec966825963945f67c9b3b54b8c50d95e28b
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-25 11:59:13 +0000

    trying to fix working directory 2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -32,13 +32,13 @@ listen 8080, :tcp_nopush => true
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/home/chris/Rails/chrisbim2ree/tmp/pids/unicorn.pid"
+pid "/home/chris/Rails/chrisbim2ree/current/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/chris/Rails/chrisbim2ree/log/unicorn.stderr.log"
-stdout_path "/home/chris/Rails/chrisbim2ree/log/unicorn.stdout.log"
+stderr_path "/home/chris/Rails/chrisbim2ree/current/log/unicorn.stderr.log"
+stdout_path "/home/chris/Rails/chrisbim2ree/current/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
