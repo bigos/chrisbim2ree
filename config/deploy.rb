170d812d9fea88d11186adebad306ac9d15430af
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-25 07:09:32 +0000

    trying different deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,49 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'yaml'
+require "rvm/capistrano"
+require "bundler/capistrano"
+secrets = YAML.load_file( "#{ File.dirname(__FILE__)}/secret.yml")
+
+set(:user) { secrets['deployment']['user'] }
+set(:password) { secrets['deployment']['password'] }
+set :application, "chrisbim2ree"
+set :app_path, "/home/#{user}/testCapistrano/#{application}"
+
+set :scm, "git"
+set :branch, "master"
+set :repository,  "git@github.com:bigos/chrisbim2ree.git"
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :use_sudo, false
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "188.226.167.41"                                # Your HTTP server, Apache/etc
+role :app, "188.226.167.41"                                # This may be the same as your `Web` server
+role :db,  "188.226.167.41", :primary => true              # This is where Rails migrations will run
+
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
