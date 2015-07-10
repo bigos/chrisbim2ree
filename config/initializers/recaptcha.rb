9ef591b4b5f3dcb7d15e34d583636e1ab361ed30
Jacek Podkanski <ruby.object@googlemail.com>
2015-07-10 19:26:41 +0100

    upgrade

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,4 +2,7 @@ Recaptcha.configure do |config|
   config.public_key  = '6Ld5Md8SAAAAALwEXCU5CIwkQvtFTHWQai6MYBR3'
   config.private_key = '6Ld5Md8SAAAAAEtTuK8iCHPzKXtLPsSCW7lwpEPU'
   #config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  config.api_version = 'v2'
 end
