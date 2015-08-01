006bf2d2d943c05366f61fb1bf0577c8f9df6930
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-01 23:21:39 +0100

    contact messages improvements

--- Gemfile
+++ Gemfile
@@ -21,7 +21,9 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails', '~> 2.3.0'
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+gem 'test-unit'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -2,10 +2,18 @@ require 'yaml'
 
 class ContactMessagesController < InheritedResources::Base
   before_filter :require_admin, :except => [:new, :create, :show]
+
+  def index
+    @contact_messages = ContactMessage.where('id >= 1').order('id desc')
+    respond_to do |format|
+      format.html
+    end
+  end
+
   def create
     @contact_message = ContactMessage.new(params[:contact_message])
 
-    if verify_recaptcha(:model => @contact_message, :message => "Oh! It's error with reCAPTCHA!") 
+    if verify_recaptcha(:model => @contact_message, :message => "Oh! It's error with reCAPTCHA!")
       respond_to do |format|
         if @contact_message.save
           @contact_message.deliver_contact_message!
