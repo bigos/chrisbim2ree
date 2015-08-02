f73ac3f4bd00bb3e2156c9c66e62575332997d01
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-02 13:53:57 +0100

    trying new setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,8 +47,7 @@ set :deploy_to, "/home/#{fetch(:user)}/Rails/#{fetch(:application)}"
 # Default value for :pty is false
 # set :pty, true
 
-
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/secret.yml')
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/assets', 'public/ckeditor_assets', 'public/system')
 
