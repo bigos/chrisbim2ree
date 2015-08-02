aafe3abe8a60c6973e21a1d62c4bac3ec3e68d96
Jacek Podkanski <ruby.object@googlemail.com>
2015-08-02 14:04:12 +0100

    zzzzz

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, "chrisbim2ree"
 set :repo_url, "git@github.com:bigos/chrisbim2ree.git"
 
 # change the IP to production server
-server '192.168.1.78', roles: [:web, :app, :db], primary: true
+server 'chrisbeard-images.com', roles: [:web, :app, :db], primary: true
 
 set :user, "#{secrets['deployment']['user'] }"
 set :puma_threads,    [4, 16]
