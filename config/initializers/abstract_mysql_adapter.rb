e401539ba15181db51f869f6b3044cd91bccb6f4
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-21 21:25:04 +0100

    mysql fix

--- config/initializers/abstract_mysql_adapter.rb
+++ config/initializers/abstract_mysql_adapter.rb
@@ -1,10 +1,8 @@
 # config/initializers/abstract_mysql_adapter.rb
-# ActiveSupport.on_load(:active_record) do
-#   class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-#     NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-#   end
-# end
 
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+# following code fixes issues with code created for previous values of Mysql
+ActiveSupport.on_load(:active_record) do
+  class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+    NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+  end
 end
