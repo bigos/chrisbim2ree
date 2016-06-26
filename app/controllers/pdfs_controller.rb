97e28eb7f4e00e7f073f2461d510eb6ef62a7165
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-26 23:37:52 +0100

    removed pdf code, starting again

--- app/controllers/pdfs_controller.rb
+++
@@ -1,3 +0,0 @@
-class PdfsController < InheritedResources::Base
-end
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160626214209) do
+ActiveRecord::Schema.define(:version => 20160428161542) do
 
   create_table "cart_items", :force => true do |t|
     t.integer  "shopping_cart_id"
@@ -76,16 +76,6 @@ ActiveRecord::Schema.define(:version => 20160626214209) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "pdfs", :force => true do |t|
-    t.string   "title"
-    t.string   "attachment_file_name"
-    t.string   "attachment_content_type"
-    t.integer  "attachment_file_size"
-    t.datetime "attachment_updated_at"
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
   end
 
   create_table "photos", :force => true do |t|
